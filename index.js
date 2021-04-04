9b041793938b71f83c509f2e7da56e8640d537cd
Elsa Brown <elsambrown@gmail.com>
2021-04-04 09:05:25 -0400

    wip

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-import Pages from './pages';
-
 const CMS = 'https://coopfund-61b6f.firebaseio.com/data.json?auth=zeRFmSs9gbS1cS9mQbPHxXTZENergP3PZdpkEYOP';
 const fields = ['members', 'balance', 'projects'];
 
 const render = (data) => {
-	let i = 0;
-	for (var page in Pages) {
-		const id = `page-0${i}`;
-		document.getElementById(id).innerHTML = Pages[page];
-		i++
-	};
-
 	fields.forEach(field => {
 		document.getElementById(`js-${field}`).innerHTML = data[0][field];
 	});
@@ -23,4 +14,4 @@ const init = () => {
 		.catch(err => {console.warn(err)});
 };
 
-export default init();
+init();
